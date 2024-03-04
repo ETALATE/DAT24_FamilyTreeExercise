a5ba6ecc13c66640bdf9aa6b146ab2e4339ffcdf
ETALATE <84042296+ETALATE@users.noreply.github.com>
2024-03-04 10:32:12 +0100

    løsning med rettelse og ekstra kode i main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         FamilyTree myFamilyTree = new FamilyTree();
-        myFamilyTree.buildTree();
+        System.out.println("En person: " + myFamilyTree.getCurrentName());
+        System.out.println("Personens mor: " + myFamilyTree.lookUpParentName("mother"));
+        myFamilyTree.moveToAncestor("mother");
         System.out.println("En person: " + myFamilyTree.getCurrentName());
         System.out.println("Personens mor: " + myFamilyTree.lookUpParentName("mother"));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        myFamilyTree.moveToAncestor("mother");
+//                System.out.println("En person: "+myFamilyTree.getCurrentName());
+//                System.out.println("Personens mor: "+myFamilyTree.lookUpParentName("mother"));
+//                myFamilyTree.moveToAncestor("mother");
+//                System.out.println("Personens mor: "+myFamilyTree.lookUpParentName("mother"));
